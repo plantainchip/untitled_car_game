f84a6fa3ef55b6e0be361ae08f94dcfa37110c84
Maitri Sarif <maitrisarif@pop-os.localdomain>
2025-07-29 15:57:47 -0400

    main demo file ext

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import kaplay from "kaplay";
 import "kaplay/global"; // uncomment if you want to use without the k. prefix
 
 import tutorial from "./scenes/tutorial";
-import demo from "./scenes/demo";
+import demo from "./scenes/demo.js";
 
 
 // const k = kaplay();
